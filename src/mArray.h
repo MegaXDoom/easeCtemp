aeb850f3bc684da985b392efbd5bd54448804ff0
MegaXDoom <MegaXDoomplayz@gmail.com>
2023-12-08 11:07:59 -0500

    fixing stuff

--- src/mArray.h
+++ src/mArray.h
@@ -4,9 +4,18 @@
 
 typedef void *data;
 
-typedef struct _mArray {
+typedef struct _mArray
+{
   data *start;
   unsigned int size;
 } mArray;
+
+mArray *new_mArray();
+int resize_mArray(mArray *array, unsigned int length);
+int insert_mArray(mArray *array, unsigned int index, data *toAdd);
+int remove_mArray(mArray *array, unsigned int index);
+int set_mArray(mArray *array, unsigned int index, data *toAdd);
+data *get_mArray(mArray *array, unsigned int toGet);
+int free_mArray(mArray *array);
 
 #endif
